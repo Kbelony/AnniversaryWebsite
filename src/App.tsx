d03442c7af426d4772554373cbf23c69cce7d487
Kalvin <kalvin.sgb@outlook.com>
2024-10-13 02:18:41 -0700

    🚧 : creating timer + add confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import { Button } from "@/components/ui/button";
 import "./assets/scss/style.scss";
+import { useState, useEffect } from "react";
+import ReactConfetti from "react-confetti";
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState("");
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date();
+      const target = new Date(now.getFullYear(), 9, 24); // 9 représente octobre (0-indexé)
+      if (now > target) target.setFullYear(target.getFullYear() + 1);
+
+      const diff = target.getTime() - now.getTime();
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+      setTimeLeft(`${days}j ${hours}h ${minutes}m ${seconds}s`);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const handleConfetti = () => {
+    setShowConfetti(true);
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button variant="outline">Button</Button>
+    <div className="hero_section mt-64 xl:mt-80">
+      {showConfetti && <ReactConfetti />}
+      <div className="flex text-center flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-4">Tic Tac ⏰ !</h1>
+        <p className="px-3 xl:px-96 underline">
+          Elle a 22 ans ? Apparement non pas encore il lui reste encore un peu
+          de temps,le site ne s'ouvrira pas tant qu'elle n'aura pas soufflé ses
+          bougies alors
+        </p>
+        <h1 className="text-3xl md:text-7xl mt-6 font-bold mb-8">{timeLeft}</h1>
+        <Button variant="outline" onClick={handleConfetti}>
+          En attentant 🎉
+        </Button>
+      </div>
     </div>
   );
 }
